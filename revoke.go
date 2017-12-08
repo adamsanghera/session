d7b710b4c1b6753bbb25321b9d07c1c1f000a894
Adam <as8513@nyu.edu>
2017-12-08 18:22:32 -0500

    added another couple comment lines to revoke.go

--- revoke.go
+++ revoke.go
@@ -5,6 +5,8 @@ import (
 )
 
 // Revoke gives you the ability to revoke a given session token.
+// returns true iff (1) privilege was revoked (2) privilege was never assigned.
+// returns false iff there's some deeper error with redis.
 func (sesh *Session) Revoke(uname string) (bool, error) {
 	// If res is 1, it means that it was deleted. 0 means not-found.
 	// err will only be non-nil if there's some deeper issue (lost connection, etc.)
